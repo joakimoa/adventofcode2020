3161dc16a25c3d651e0a57552967da7b747d185b
Joakim Almgren <joakim.oa@gmail.com>
2020-12-09 10:44:24 +0100

    Add day 8, problem 2

--- day8/day8.exs
+++ day8/day8.exs
@@ -1,7 +1,12 @@
 defmodule Gameboy do
-  # return: Immediately before any instruction is executed a second time, what value is in the accumulator?
-  def recursion_debug(instructions) do
+  # return acc when recursion is found
+  def acc_when_recursion(instructions) do
     execute(instructions, 0, 0, MapSet.new())
+  end
+
+  # return acc with a single swapped jmp or nop and program exits
+  def acc_when_correcting(instructions) do
+    execute_correcting(instructions, 0, 0, MapSet.new(), 4) # hard-coded first swp...
   end
 
   defp execute(ins, pos, acc, vis) do
@@ -15,8 +20,33 @@ defmodule Gameboy do
         {"acc", acc_amount} -> {pos + 1, acc + String.to_integer(acc_amount)}
         {"nop", _} -> {pos + 1, acc}
       end
-      # IO.inspect {pos, acc, vis}
       execute(ins, pos, acc, vis)
+    end
+  end
+
+  defp execute_correcting(ins, pos, acc, vis, swp) do
+    # IO.inspect {elem(ins, pos), pos, acc, vis, swp}
+    # IO.inspect {pos, tuple_size(ins)}
+    if (pos >= tuple_size(ins)) do
+      acc #1245
+    else
+      if (MapSet.member?(vis, pos)) do
+        # branch, new jmp to nop or nop to jmp
+        {_, swp} = Enum.with_index(Tuple.to_list(ins))
+          |> Enum.find(&( elem(&1, 1) > swp && String.contains?(elem(elem(&1, 0),0), ["jmp", "nop"]) ))
+        execute_correcting(ins, 0, 0, MapSet.new(), swp)
+      else
+        vis = MapSet.put(vis, pos)
+        # {pos, acc} = case elem(ins, pos) do
+        {pos, acc} = case {elem(ins, pos), pos == swp} do
+          {{"jmp", new_pos}, false} -> {pos + String.to_integer(new_pos), acc}
+          {{"acc", acc_amount}, false} -> {pos + 1, acc + String.to_integer(acc_amount)}
+          {{"nop", _}, false} -> {pos + 1, acc}
+          {{"jmp", _}, true} -> {pos + 1, acc}
+          {{"nop", new_pos}, true} -> {pos + String.to_integer(new_pos), acc}
+        end
+        execute_correcting(ins, pos, acc, vis, swp)
+      end
     end
   end
 end
@@ -31,4 +61,5 @@ end
 
 instructions = List.to_tuple(instructions)
 
-IO.inspect Gameboy.recursion_debug(instructions)
+IO.inspect Gameboy.acc_when_recursion(instructions)
+IO.inspect Gameboy.acc_when_correcting(instructions)
